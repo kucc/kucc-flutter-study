b5215fc6629c49d39850bb341dd33a0d29dd4fc4
BriceKim <95173953+Brice-Kim@users.noreply.github.com>
2022-10-07 18:53:51 +0900

    Dialog homework

--- main.dart
+++ main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+      home: MyApp()
+  ),
+  );
 }
 
 class bottom extends StatelessWidget {
@@ -24,25 +27,71 @@ class bottom extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name = ['김영숙','홍길동','피자집'];
+  var like = [0,0,0];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Text('안녕'),
+            onPressed: (){
+              showDialog(context: context, builder: (context){
+                return Dialog(child:
+                Container(
+                  padding: EdgeInsets.all(40),
+                  width: 400,
+                  height:300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Contact', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500,),),
+                      TextField(decoration: InputDecoration(hintText: 'Enter your phone number'),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(onPressed: (){}, child: Text('Cancel'),),
+                          TextButton(onPressed: (){}, child: Text('OK'),),],
+                      )
+                    ],
+                  ),
+                )
+                );
+              });
+            },
+          ),
         appBar: AppBar(),
         body: ListView.builder(
             itemCount: 3,
             itemBuilder: (c,i){
               return ListTile(
-                leading: Icon(Icons.account_circle, size: 50,),
-                title: Text('홍길동', style: TextStyle(fontSize: 20,),
+                leading: Container(
+                  child:
+                  Text(like[i].toString()),
+                ),
+                title: Text(name[i], style: TextStyle(fontSize: 20,),
+                ),
+                trailing: TextButton(style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.blue),),
+                  child: Text('좋아요', style: TextStyle(color: Colors.white,)),
+                  onPressed: (){
+                    setState(() {
+                      like[i]++;
+                    });
+                  },
                 ),
               );
             }, ),
         bottomNavigationBar: bottom(),
-          ),
-        );
+          );
   }
 }
 
