145e7404d4ef32377d50ec1e06d1cf48e2bb5fa9
keo88 <keo.kim0808@gmail.com>
2022-10-07 22:30:08 +0900

    feat: textfield added to the dialog

--- lib/views/show_dialog_screen.dart
+++ lib/views/show_dialog_screen.dart
@@ -49,40 +49,43 @@ class _ShowDialogScreenState extends State<ShowDialogScreen> with SingleTickerPr
       var width = MediaQuery.of(context).size.width;
       showDialog(context: context, builder: (context) {
         return Center(
-          child: Container(
-            width: width * 0.8,
-            height: height * 0.8,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0)
-            ),
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Expanded(
-                  child: RotationTransition(
-                    turns: _spinAnimation,
-                    child: FlutterLogo(size: 0.5 * width)
-                  ),
-                ),
-                const SizedBox(
-                  height: 200,
-                  child: Text(
-                    '플\$러❤️터\n스#터@디\$\n\n즉♠︎시♤가&입',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 24,
+          child: Card(
+            child: Container(
+              width: width * 0.8,
+              height: height * 0.8,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0)
+              ),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: RotationTransition(
+                      turns: _spinAnimation,
+                      child: FlutterLogo(size: 0.5 * width)
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(onPressed: () {}, child: Text('싫은')),
-                    ElevatedButton(onPressed: () {}, child: Text('좋은')),
-                  ],
-                )
-              ],
+                  const SizedBox(
+                    height: 200,
+                    child: Text(
+                      '플\$러❤️터\n스#터@디\$\n\n즉♠︎시♤가&입',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 80, child: TextField()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(onPressed: () {}, child: Text('싫은')),
+                      ElevatedButton(onPressed: () {}, child: Text('좋은')),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         );
