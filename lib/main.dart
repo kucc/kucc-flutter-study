0a92c61e15da6cd9b1214add397409c2a876a752
LHJ <jjs01hwang@gmail.com>
2022-10-07 13:42:16 +0900

    FEATURE : 3주차 - 2번째 과제

--- lib/main.dart
+++ lib/main.dart
@@ -4,66 +4,41 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: ListView(
-          children: [
-            Profile(),
-            Profile(),
-            Profile(),
-          ],
-        ),
-        bottomNavigationBar: BottomTab(),
-      ),
-    );
-  }
-}
-
-class Profile extends StatelessWidget {
-  const Profile({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          Icons.account_circle,
-          size: 40,
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Text(
-          '홍길동',
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class BottomTab extends StatelessWidget {
-  const BottomTab({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: SizedBox(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.phone),
-            Icon(Icons.chat),
-            Icon(Icons.contact_page),
-          ],
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text(like[index].toString()),
+              title: Text(
+                name[index],
+              ),
+              trailing: ElevatedButton(
+                child: Text('좋아요'),
+                onPressed: () {
+                  print(like[index]);
+                  setState(() {
+                    like[index]++;
+                  });
+                },
+              ),
+            );
+          },
         ),
       ),
     );
