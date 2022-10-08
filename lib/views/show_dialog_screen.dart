7db3d081701e95228c5d7286090fc79198ab51b2
keo88 <keo.kim0808@gmail.com>
2022-10-08 13:19:48 +0900

    chore: reformat codes

--- lib/views/show_dialog_screen.dart
+++ lib/views/show_dialog_screen.dart
@@ -7,8 +7,8 @@ class ShowDialogScreen extends StatefulWidget {
   State<ShowDialogScreen> createState() => _ShowDialogScreenState();
 }
 
-class _ShowDialogScreenState extends State<ShowDialogScreen> with SingleTickerProviderStateMixin {
-
+class _ShowDialogScreenState extends State<ShowDialogScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animController;
   late Animation<double> _spinAnimation;
 
@@ -19,10 +19,8 @@ class _ShowDialogScreenState extends State<ShowDialogScreen> with SingleTickerPr
       duration: const Duration(milliseconds: 500),
     );
     _animController.repeat(reverse: false);
-    _spinAnimation = CurvedAnimation(
-      parent: _animController,
-      curve: Curves.linear
-    );
+    _spinAnimation =
+        CurvedAnimation(parent: _animController, curve: Curves.linear);
 
     super.initState();
   }
@@ -36,7 +34,7 @@ class _ShowDialogScreenState extends State<ShowDialogScreen> with SingleTickerPr
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text ('HW2')),
+      appBar: AppBar(title: const Text('HW2')),
       floatingActionButton: FloatingActionButton(
         onPressed: showSpinningDialog(context),
       ),
@@ -47,50 +45,49 @@ class _ShowDialogScreenState extends State<ShowDialogScreen> with SingleTickerPr
     return () {
       var height = MediaQuery.of(context).size.height;
       var width = MediaQuery.of(context).size.width;
-      showDialog(context: context, builder: (context) {
-        return Center(
-          child: Card(
-            child: Container(
-              width: width * 0.8,
-              height: height * 0.8,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0)
-              ),
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: RotationTransition(
-                      turns: _spinAnimation,
-                      child: FlutterLogo(size: 0.5 * width)
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 200,
-                    child: Text(
-                      '플\$러❤️터\n스#터@디\$\n\n즉♠︎시♤가&입',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 80, child: TextField()),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      showDialog(
+          context: context,
+          builder: (context) {
+            return Center(
+              child: Card(
+                child: Container(
+                  width: width * 0.8,
+                  height: height * 0.8,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
                     children: [
-                      ElevatedButton(onPressed: () {}, child: Text('싫은')),
-                      ElevatedButton(onPressed: () {}, child: Text('좋은')),
+                      Expanded(
+                        child: RotationTransition(
+                            turns: _spinAnimation,
+                            child: FlutterLogo(size: 0.5 * width)),
+                      ),
+                      const SizedBox(
+                        height: 200,
+                        child: Text(
+                          '플\$러❤️터\n스#터@디\$\n\n즉♠︎시♤가&입',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 80, child: TextField()),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(onPressed: () {}, child: Text('싫은')),
+                          ElevatedButton(onPressed: () {}, child: Text('좋은')),
+                        ],
+                      )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-          ),
-        );
-      });
+            );
+          });
     };
   }
 }
-
